f10d86b7252ead642621c407006b73ae1fd92bb9
Clementine <clementine@phung.fr>
2024-03-28 18:02:16 +0100

    updates diff memory+networks for each asset, still not working

--- modules/trading_agent.py
+++ modules/trading_agent.py
@@ -13,7 +13,8 @@ from modules.memory import Memory
 
 class TradingAgent:
 
-    def __init__(self, state_size, num_actions, qnet=Q_network, 
+    def __init__(self, state_size, num_actions, assets,
+                 qnet=Q_network, 
                  batch_size = 64, learning_rate=1e-3,
                  tau = 2e-3, gamma=0.95, device='cpu',
                  learning_freq=5, seed = 123) -> None:
@@ -21,7 +22,7 @@ class TradingAgent:
         self.state_size = state_size
         self.num_actions = num_actions
 
-        self.memory = Memory(batch_size*2, seed=seed)
+        self.map_assets = {asset: id for id, asset in enumerate(assets)}
 
         self.batch_size = batch_size
         self.learning_rate = learning_rate
@@ -29,8 +30,10 @@ class TradingAgent:
         self.gamma = gamma
         self.tau = tau
 
-        self.qnet = qnet(state_size, num_actions)
-        self.qnet_target = qnet(state_size, num_actions)
+        self.qnets = [qnet(state_size, num_actions) for asset in assets]
+        self.qnets_target = [qnet(state_size, num_actions) for asset in assets]
+
+        self.memory = [Memory(batch_size*2, seed=seed) for asset in assets]
 
         self.device = device
         self.seed = seed
@@ -39,22 +42,22 @@ class TradingAgent:
         self.step_count = 0
 
         self.loss_fun = nn.MSELoss()
-        self.optimiser = optim.Adam(self.qnet.parameters(), lr=learning_rate)
+        self.optimiser = [optim.Adam(qnet.parameters(), lr=learning_rate) for qnet in self.qnets]
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, asset, state, action, reward, next_state, done):
 
         # save tuple in memory
-        self.memory.add(state, action, reward, next_state, done)
+        self.memory[self.map_assets[asset]].add(state, action, reward, next_state, done)
         
         # learn at learning_freq (every learning_freq days)
         self.step_count += 1
         if self.step_count % self.learning_freq == 0:
-            if len(self.memory) > self.batch_size:
-                experiences = self.memory.sample(self.batch_size)
-                self.learn(experiences)
+            if len(self.memory[self.map_assets[asset]]) > self.batch_size:
+                experiences = self.memory[self.map_assets[asset]].sample(self.batch_size)
+                self.learn(asset, experiences)
 
 
-    def act(self, state, epsilon):
+    def act(self, asset, state, epsilon):
         """Selects action. 
 
         With proba (1-epsilon) select action having the highest score.
@@ -62,6 +65,8 @@ class TradingAgent:
 
         Parameters
         ----------
+        asset: str
+            Asset name for which we should learn
         state: list
             List of observations constituting the state. List is composed of the 
             history and the predicted value for the current period.
@@ -75,19 +80,19 @@ class TradingAgent:
         """
         state = torch.from_numpy(np.array(state, dtype=np.float32).reshape(1, -1))
 
-        self.qnet.eval() # set model in evaluation mode
+        self.qnets[self.map_assets[asset]].eval() # set model in evaluation mode
         if np.random.rand() > epsilon:
             with torch.no_grad():
-                scores_actions = self.qnet(state)
+                scores_actions = self.qnets[self.map_assets[asset]](state)
                 action = np.argmax(scores_actions.cpu().data.numpy())
         else:
             action = np.random.randint(self.num_actions)
-        self.qnet.train() # set model in evaluation mode
+        self.qnets[self.map_assets[asset]].train() # set model in evaluation mode
         
         return int(action)
 
 
-    def learn(self, experiences):
+    def learn(self, asset, experiences):
 
         # unpack states, actions, rewards, next_states and dones
         states = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(self.device)
@@ -97,10 +102,10 @@ class TradingAgent:
         dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
 
 
-        pred_Q_vals = self.qnet(states).gather(1, actions)
+        pred_Q_vals = self.qnets[self.map_assets[asset]](states).gather(1, actions)
 
         # get max Q values for (s_{t+1}, a_{t+1}) from target model
-        pred_Q_vals_next_target_scores = self.qnet_target(next_states).detach()
+        pred_Q_vals_next_target_scores = self.qnets_target[self.map_assets[asset]](next_states).detach()
         pred_Q_vals_next_target = pred_Q_vals_next_target_scores.max(1)[0].unsqueeze(1)
 
         # Q target for current states
@@ -109,17 +114,18 @@ class TradingAgent:
         # compute loss + minimize it
         loss = self.loss_fun(pred_Q_vals, pred_Q_vals_target)
 
-        self.optimiser.zero_grad()
+        self.optimiser[self.map_assets[asset]].zero_grad()
         loss.backward()
-        self.optimiser.step()
+        self.optimiser[self.map_assets[asset]].step()
 
         # update target network
-        self.soft_update(self.qnet, self.qnet_target, self.tau)
+        self.soft_update(self.qnets[self.map_assets[asset]], self.qnets_target[self.map_assets[asset]], self.tau)
     
     def soft_update(self, local_model, target_model, tau):
         for local_param, target_param in zip(local_model.parameters(), target_model.parameters()):
             target_param.data.copy_(tau*local_param + (1-tau)*target_param.data)
 
     def save_models(self, folder="/"):
-        filename = os.path.join(folder, "trained_agent_model.pth")
-        torch.save(self.qnet.state_dict(), filename)
+        for asset in self.map_assets.keys():
+            filename = os.path.join(folder, "trained_agent_model_"+asset+".pth")
+            torch.save(self.qnets[self.map_assets[asset]].state_dict(), filename)
